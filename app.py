bf0ff5a09b14dc801991fccddf008395864b0694
Aabhas Kumar Jha <abhasjha03@gmail.com>
2023-10-22 15:03:22 +0530

    update

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask 
 
 app = Flask(__name__)
+app.debug = False
 
 @app.route("/")
 def landing():
 	return "life of a mavrick"
 
-if __name__ == "__main__":
-	app.run(debug = False)
