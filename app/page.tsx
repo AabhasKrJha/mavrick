db50d3ee91bb4bbab7747312beb53293eb086335
Aabhas Kumar Jha <abhasjha03@gmail.com>
2024-06-04 19:45:30 +0530

    simple comming soon page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <div className="bg-[conic-gradient(var(--tw-gradient-stops))] from-black via-black to-black relative h-full overflow-hidden">
           <div className="absolute inset-0 bg-[url(https://grainy-gradients.vercel.app/noise.svg)] opacity-25 brightness-100 contrast-150 flex flex-col items-center justify-center gap-5">
             <h1 className="text-7xl font-black text-white">Life of a Mavrick</h1>
-            <Badge>Comming Soon</Badge>
+            <Badge className="text-md">Comming Soon</Badge>
           </div>
         </div>
       </div> 
